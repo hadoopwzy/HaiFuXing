d3c3a25fa1c351fd60c404028971081ceb8dbe67
汪紫悦 <print_wzy@163.com>
2023-07-13 10:20:12 +0800

    111

--- assets/js/haifuxing_job_content.js
+++ assets/js/haifuxing_job_content.js
@@ -15,10 +15,6 @@
     if(!id){
         id = 1;
     }
-    var job_type_id = GetQueryString('id');
-    if(!job_type_id){
-        job_type_id = 1;
-    }
 
     $.ajax({ // 使用POST方法向host+/getIndexData发送JSON数据{"params":{}}，并且设置请求的数据类型为 JSON
         type: "POST",
@@ -27,85 +23,54 @@
         data: JSON.stringify({ "params": {"id":id} }),
         success: function (retdata) {  // 请求成功后的回调
             console.log(retdata);  // 输出服务器响应结果
-            var jobs = retdata['result']['jobs'];
+            var job_contents = retdata['result']['job_contents'];
 // ===================================岗位内容=======================================>
-            jobs.forEach(function(job,index){
-                const job_contnet_html='<div class="details-item">\
+            job_contents.forEach(function(job_content,index){
+                const job_contnet_html='<div class="details-item p-5">\
                                             <div class="details-digital">\
-                                                <h3>'+job.name+'</h3>\
-                                                <p>发布日期：'+job.show_date+'</p>\
-                                                <p>工作地点：'+job.state+' '+job.city+'</p>\
-                                                <p>薪资：'+job.salary+'</p>\
+                                                <h3>'+job_content.name+'</h3>\
+                                                <p>发布日期：'+job_content.show_date+'</p>\
+                                                <p>工作地点：'+job_content.state+' '+job_content.city+'</p>\
+                                                <p>薪资：'+job_content.salary+'</p>\
                                                 <p>岗位要求：</p>\
                                                 <div class="job_content_html"></div>\
                                             </div>\
                                         </div>'
             $('.job_content').append(job_contnet_html)
-            $('.job_content_html').html(job.content)
-
+            $('.job_content_html').html(job_content.content)
+            
+// ===================================推荐岗位=======================================>
+            var job_type_id = job_content.job_type_id
+            $.ajax({ // 使用POST方法向host+/getIndexData发送JSON数据{"params":{}}，并且设置请求的数据类型为 JSON
+                type: "POST",
+                url: host + '/getHFXJobData',
+                contentType: "application/json",
+                data: JSON.stringify({ "params": {"job_type_id":job_type_id} }),
+                success: function (retdata) {  // 请求成功后的回调
+                    console.log(retdata);  // 输出服务器响应结果
+                    var jobs = retdata['result']['jobs'];
+                    jobs.forEach(function(job,index){
+                        const job_recommenda_html='<li>\
+                                                        <a class="highlight" href="haifuxing_job_content.html?id='+job.id+'">'+job.name+'</a>\
+                                                    </li>'
+                        if (job_content.name != job.name){
+                            $('.job_recommend').append(job_recommenda_html)
+                        }
+                        
+                    })
+                },
+                    
+                // 请求失败，返回异常信息
+                error: function (data) {
+                    console.log(data);
+                }
+            });
             })
         },
-            
         // 请求失败，返回异常信息
         error: function (data) {
             console.log(data);
         }
     });
-    $.ajax({ // 使用POST方法向host+/getIndexData发送JSON数据{"params":{}}，并且设置请求的数据类型为 JSON
-        type: "POST",
-        url: host + '/getHFXJobData',
-        contentType: "application/json",
-        data: JSON.stringify({ "params": {"job_type_id":job_type_id} }),
-        success: function (retdata) {  // 请求成功后的回调
-            console.log(retdata);  // 输出服务器响应结果
-            var jobs = retdata['result']['jobs'];
-// ===================================岗位内容=======================================>
-            jobs.forEach(function(job,index){
-                const job_contnet_html='<div class="details-item">\
-                                            <div class="details-digital">\
-                                                <h3>'+job.name+'</h3>\
-                                                <p>发布日期：'+job.show_date+'</p>\
-                                                <p>工作地点：'+job.state+' '+job.city+'</p>\
-                                                <p>薪资：'+job.salary+'</p>\
-                                                <p>岗位要求：</p>\
-                                                <div class="job_content_html"></div>\
-                                            </div>\
-                                        </div>'
-            $('.job_content').append(job_contnet_html)
-            $('.job_content_html').html(job.content)
-
-            const job_recommend_html='<h3>其他岗位</h3>\
-                                    <ul>\
-                                        <li>\
-                                            <a href="#">Design</a>\
-                                            <span>(1)</span>\
-                                        </li>\
-                                        <li>\
-                                            <a href="#">Lifestyle</a>\
-                                            <span>(4)</span>\
-                                        </li>\
-                                        <li>\
-                                            <a href="#">Campaigns</a>\
-                                            <span>(3)</span>\
-                                        </li>\
-                                        <li>\
-                                            <a href="#">Device</a>\
-                                            <span>(6)</span>\
-                                        </li>\
-                                        <li>\
-                                            <a href="#">Internet</a>\
-                                            <span>(8)</span>\
-                                        </li>\
-                                    </ul>'
-        // $('.job_recommend').append(job_recommend_html)
-
-            })
-        },
-            
-        // 请求失败，返回异常信息
-        error: function (data) {
-            console.log(data);
-        }
-    });
-
+    
 })(jQuery);;
