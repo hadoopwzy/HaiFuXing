3b8c32e91ee7920fcaf20c518d59554b8959b326
汪紫悦 <print_wzy@163.com>
2023-07-24 16:57:16 +0800

    11

--- assets/js/haifuxing_job.js
+++ assets/js/haifuxing_job.js
@@ -88,10 +88,10 @@
 
     CreateJob(job_type_id)
 
-    $(".job_type_a").on('click',function(){
+    $(".job_type_a job_type_link").on('click',function(){
+        $('.job').empty() // 清空元素内容
         job_type_id = $(this).attr("id").match(/\d/g)[0]
         console.log(job_type_id)
-        $('.job').empty() // 清空元素内容
         CreateJob(job_type_id)
         $('#accordionExample').children().find('.highlight').removeClass('highlight')
         $(this).addClass('highlight')
